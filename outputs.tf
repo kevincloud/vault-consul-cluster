6e75910e019bd6017108655130901a47cb5f43a9
Kevin Cochran <kcochran@hashicorp.com>
2019-05-20 20:01:56 -0400

    Initial commit

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
-output "consul-cluster" {
-    value = ""
+output "consul-server-1" {
+    value = "ssh -i ~/keys/${var.key_pair}.pem ubuntu@${aws_instance.consul-server-1.public_ip}"
 }
+
+output "consul-server-2" {
+    value = "ssh -i ~/keys/${var.key_pair}.pem ubuntu@${aws_instance.consul-server-2.public_ip}"
+}
+
+output "consul-server-3" {
+    value = "ssh -i ~/keys/${var.key_pair}.pem ubuntu@${aws_instance.consul-server-3.public_ip}"
+}
+
